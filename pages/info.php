47602233663c84f9bc1f4ca80b89c1884d6d6360
Pavel Cvrcek <jasnapaka@jasnapaka.com>
2014-06-29 22:50:35 +0200

    Info stránka o pluginu přesunuta do samostatného souboru.

---
+++ pages/info.php
@@ -0,0 +1,5 @@
+<h2>Správa objektů</h2>
+
+<p>Plugin pro WordPress, který umožňuje spravovat body v mapě a základní informace o nich.</p>
+
+<p>Zdrojový kód plugin je <a href="https://github.com/JasnaPaka/wpcity">k dispozici na GitHubu</a>.</p>
--- wpcity.php
+++ wpcity.php
@@ -53,13 +53,7 @@ function wpCityMenu(){
 }
 
 function wpCityMenuPageCallback(){
-?>
-
-<h2>Správa objektů</h2>
-<p>Plugin pro WordPress, který umožňuje spravovat body v mapě a základní informace o nich.</p>
-<p>Zdrojový kód plugin je <a href="https://github.com/JasnaPaka/wpcity">k dispozici na GitHubu</a>.</p>
-
-<?php
+	require_once("pages/info.php");
 }
 
 /** Přidáme podnabídku */
