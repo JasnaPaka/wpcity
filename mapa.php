3917396aeb01cf253fd6e599b876899a4de64fc0
Pavel Cvrcek <jasnapaka@jasnapaka.com>
2014-07-06 22:04:26 +0200

    U bodu v mapě zobrazovat hlavní fotku.

--- mapa.php
+++ mapa.php
@@ -17,17 +17,30 @@ function kv_MapaLegenda() {
 
 function kv_MapaData() {
 	global $wpdb;
+	
+	$uploadDir = wp_upload_dir();
 	$output = "";
 	
-	$rows = $wpdb->get_results("SELECT kv_objekt.*, kv_kategorie.ikona FROM kv_objekt INNER JOIN kv_kategorie ON kv_objekt.kategorie = kv_kategorie.id ORDER BY kategorie, nazev");
-	foreach($rows as $row) {
+	$wpdb->show_errors();	
+	$rows = $wpdb->get_results("SELECT kv.*, kv_kategorie.ikona,
+		(SELECT img_thumbnail FROM kv_fotografie WHERE objekt = kv.id order by primarni, id) as img_thumbnail,
+		(SELECT img_large FROM kv_fotografie WHERE objekt = kv.id order by primarni, id) as img_large
+		FROM kv_objekt AS kv INNER JOIN kv_kategorie ON kv.kategorie = kv_kategorie.id 
+		ORDER BY kategorie, nazev");
+		
+	foreach($rows as $row) {		
 		if (strlen($output) > 0) {
 			$output.=",";
 		}
 		
 		$nazev = str_replace("'", "\'", $row->nazev);
 		
-		$output.= '[\'<div style="white-space:nowrap; font-family: Verdana">'.$nazev.'</div>\','.$row->latitude.','.$row->longitude.','.$row->kategorie.',\''.$row->ikona.
+		$content = "<p><strong>".$nazev."</strong></p>";
+		if ($row->img_thumbnail != null) {
+			$content = $content.'<div><a href="'.$uploadDir["baseurl"].$row->img_large.'"><img src="'.$uploadDir["baseurl"].$row->img_thumbnail.'" alt="" /"></a>';
+		}
+		
+		$output.= '[\'<div style="white-space:nowrap; font-family: Verdana">'.$content.'</div>\','.$row->latitude.','.$row->longitude.','.$row->kategorie.',\''.$row->ikona.
 		'\',\''.$nazev.'\']';
 	}
 		
