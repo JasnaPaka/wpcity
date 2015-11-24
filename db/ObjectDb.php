0958e672d0eb88129bc514e7410ef21343c8da87
Pavel Cvrček <jasnapaka@jasnapaka.com>
2015-11-24 18:12:23 +0100

    Smazané fotky neukazovat v náhodných dílech

--- db/ObjectDb.php
+++ db/ObjectDb.php
@@ -210,7 +210,7 @@ class ObjectDb extends JPDb {
 		$sql = $wpdb->prepare("SELECT obj.id, obj.nazev, fot.img_512, obj.kategorie FROM ".$this->tableName." obj 
 			INNER JOIN ".$this->dbPrefix."fotografie fot ON obj.id = fot.objekt
 			INNER JOIN ".$this->dbPrefix."kategorie kat ON obj.kategorie = kat.id 
-			WHERE fot.primarni = 1 AND fot.skryta = 0 AND obj.deleted = 0 AND obj.schvaleno = 1 AND kat.systemova = 0 AND obj.zruseno = 0 ORDER BY obj.id LIMIT 1 OFFSET %d", $randomNumber);		
+			WHERE fot.primarni = 1 AND fot.skryta = 0 AND obj.deleted = 0 AND fot.deleted = 0 AND obj.schvaleno = 1 AND kat.systemova = 0 AND obj.zruseno = 0 ORDER BY obj.id LIMIT 1 OFFSET %d", $randomNumber);		
 			
 		$results = $wpdb->get_results ($sql);
 		return $results[0];
