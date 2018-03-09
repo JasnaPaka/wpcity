75d17a6d6f2423599feb760d78d790ae0afdc841
Pavel Cvrček <jasnapaka@jasnapaka.com>
2018-03-09 18:53:26 +0100

    Oprava testu v PHP 7.2

--- tests/utils/WikidataSourceTest.php
+++ tests/utils/WikidataSourceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once $ROOT."utils/WikidataSource.php";
+include_once "utils/WikidataSource.php";
 
 use PHPUnit\Framework\TestCase;
 
@@ -11,7 +11,6 @@ class WikidataSourceTest extends TestCase
 		$source = new WikidataSource("Q20758044");
 		$this->assertTrue($source->getIsOK());
 
-		$this->assertEquals("https://cs.wikipedia.org/wiki/Pomn%C3%ADk_gener%C3%A1la_Pattona", $source->getCsWikiUrl());
 		$this->assertEquals("https://cs.wikipedia.org/wiki/Pomn%C3%ADk_gener%C3%A1la_Pattona", $source->getCsWikiUrl());
 	}
 
@@ -28,5 +27,22 @@ class WikidataSourceTest extends TestCase
 	public function testWikidataIdentifier() {
 		$this->assertEquals("Q369130", WikidataSource::getWikidataIdentifier("23944/4-1330"));
 	}
+
+	/*public function testDataForAuthors() {
+		$identifiers = array();
+		$identifiers[] = "Q446948";   // Mikoláš Aleš
+		$identifiers[] = "Q18745842"; // Břetislav Holakovský
+
+		$authors = WikidataSource::getDataForAuthors($identifiers);
+		$this->assertEquals (2, sizeof($authors));
+*/
+		/*foreach ($authors as $author) {
+			echo $author->datum_narozeni."\n";
+			$datum = DateTime::createFromFormat(DateTime::ISO8601, substr($author->datum_narozeni, 1));
+			echo $datum->format("d. m. Y")."\n";
+
+			//echo $datum."\n";
+		}*/
+	//}
 
 }
