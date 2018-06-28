a745c00b3f0812dbd92e6219811f909b0c39b8d1
Pavel Cvrček <jasnapaka@jasnapaka.com>
2018-06-28 21:02:19 +0200

    Oprava skriptů

--- sql/11.sql
+++ sql/11.sql
@@ -2,7 +2,5 @@ ALTER TABLE `{{PREFIX}}zdroj` ADD `typ` VARCHAR(255) AFTER `id`;
 ALTER TABLE `{{PREFIX}}zdroj` ADD `identifikator` VARCHAR(255) AFTER `id`;
 ALTER TABLE `{{PREFIX}}zdroj` MODIFY `nazev` VARCHAR(255);
 ALTER TABLE `{{PREFIX}}zdroj` ADD `system_zdroj` VARCHAR(255);
-
-
 UPDATE `{{PREFIX}}zdroj` SET typ = 'KNIHA', identifikator = isbn WHERE length(isbn) > 3;
 ALTER TABLE `{{PREFIX}}zdroj` DROP COLUMN isbn;
